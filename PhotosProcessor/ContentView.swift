a92463c5f077ebad44f8653b4ec25dd6c8eedfbc
wibus-wee <1596355173@qq.com>
2023-09-10 09:33:03 +0800

    Initial Commit

---
+++ PhotosProcessor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotosProcessor
+//
+//  Created by wibus on 2023/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotosProcessor/PhotosProcessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosProcessorApp.swift
+//  PhotosProcessor
+//
+//  Created by wibus on 2023/9/10.
+//
+
+import SwiftUI
+
+@main
+struct PhotosProcessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
